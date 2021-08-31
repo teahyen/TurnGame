ee22fe5e81f04a232c18d52d58be4118fb2c7dcd
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-09-01 00:34:26 +0900

    적 공격 구현

--- Assets/02.Script/EnemyAI.cs
+++ Assets/02.Script/EnemyAI.cs
@@ -6,6 +6,10 @@ public class EnemyAI : MonoBehaviour
 {
     private bool oneMyTurn = true;
 
+    public GameObject damagePos;
+    public List<GameObject> mapPos = new List<GameObject>();
+
+    
     void Update()
     {
         if (!GameManager.Instance.myTurn&&oneMyTurn)
@@ -16,12 +20,12 @@ public class EnemyAI : MonoBehaviour
     }
     IEnumerator EnemyTurn()
     {
+        int Ran = Random.Range(0, mapPos.Count);
+        GameObject jotEH = Instantiate(damagePos, mapPos[Ran].transform);
         yield return new WaitForSeconds(1f);
         GameManager.Instance.turnCount++;
         oneMyTurn = true;
         GameManager.Instance.myTurn = true;
-        
-
-
+        Destroy(jotEH);
     }
 }
--- Assets/02.Script/HitTest.cs
+++ Assets/02.Script/HitTest.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class HitTest : MonoBehaviour
 {
     public GameObject damagePos;
+    public List<GameObject> mapPos = new List<GameObject>();
+    
     void Start()
     {
-        
     }
 
     void Update()
     {
-        
+        int Ran = Random.Range(0, mapPos.Count);
+        Instantiate(damagePos, mapPos[Ran].transform);
     }
 }
