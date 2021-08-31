ad031d754631639e3edf022003c9745565d0ab14
DESKTOP-77DPHAE\ROG <kimct567@gmail.com>
2021-08-31 13:19:10 +0900

    시작 씬 + 인게임 맵 구현

---
+++ Assets/02.Script/SceneScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneScript : MonoBehaviour
+{
+    public void GameStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
